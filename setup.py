af5ed9ab84dcfa00d326d59e8241b16ec6ccdcc1
YuxinZhaozyx <1322032296@qq.com>
2020-03-24 13:20:47 +0800

    chore: prepare for 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'easycore',
-    version = '0.3.3',
+    version = '0.3.4',
     author = 'Yuxin Zhao',
     url = 'https://github.com/YuxinZhaozyx/easycore',
     packages = find_packages(),
